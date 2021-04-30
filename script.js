9da5c5305ebffbb6166dc480a6208ea1a88fe549
Artiu <71273256+Artiu@users.noreply.github.com>
2021-04-30 22:12:24 +0200

    commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById("submit").addEventListener("click",function(){
     let title = document.getElementById("title");
     let task = document.getElementById("thing");
-    document.getElementById("list").innerHTML += "<li>"+title.value+"</li><span>"+task.value+"</span>";
+    let list = document.getElementById("list");
+    list.innerHTML += "<li>"+title.value+"<button>Delete task</button></li><span>"+task.value+"</span>";
     title.value="";
     task.value="";
 });
