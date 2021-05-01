a226f9bfb2dcea40519509dcd1b365ec4831d638
Artiu <71273256+Artiu@users.noreply.github.com>
2021-05-01 15:48:14 +0200

    Added task remover

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.getElementById("submit").addEventListener("click",function(){
     let title = document.getElementById("title");
-    let task = document.getElementById("thing");
-    let list = document.getElementById("list");
-    list.innerHTML += "<li>"+title.value+"<button>Delete task</button></li><span>"+task.value+"</span>";
-    title.value="";
-    task.value="";
+    if(title.value!="")
+    {
+        let task = document.getElementById("thing");
+        let list = document.getElementById("list");
+        let l = list.children.length;
+        list.innerHTML += "<li id='"+l+"'>"+title.value+"<button onClick='removeItem("+l+")'>Delete task</button><span>"+task.value+"</span></li>";
+        title.value="";
+        task.value="";
+    }
 });
+removeItem = (i) =>
+{
+    document.getElementById(i).remove();
+}
